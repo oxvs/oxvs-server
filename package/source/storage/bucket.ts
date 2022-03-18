373509ef98a9002a44822760ce93214986d9d01f
0aoq <a0oq@outlook.com>
2022-03-18 01:07:52 +0000

    Bucket "get" Function

--- package/source/storage/bucket.ts
+++ package/source/storage/bucket.ts
@@ -1,11 +1,17 @@
+/**
+ * @file Manage the bucket and storage section of the OXVS-SERVER
+ * @author 0a_oq <hkau@oxvs.net>
+ * @version 0.0.1
+ */
+
 /// <reference path="auth.ts" />
 
 namespace bucket {
     /**
      * Upload data to server
-     * @class bucket.DataUploader
+     * @class ObjectHandler
      */
-    export class DataUploader {
+    export class ObjectHandler {
         sender: string
 
         constructor(props) {
@@ -13,19 +19,45 @@ namespace bucket {
         }
 
         /**
+         * @func ObjectHandler.upload
+         * @description Upload JSON data to storage for quick access later
          * 
          * @param {object} data - The data that will be saved as a new bucket/object
-         * @returns {Promise} Promise object returning either true or an error message
+         * @returns {Promise} Promise object returning either the ID or an error message
          */
-        public upload(data: object) {
+        public upload(data: any) {
+            // generate id
             const objectId = (performance.now().toString(36)+Math.random().toString(36)).replace(/\./g,"")
+            
+            // add sender value to data
+            data.__sender = this.sender
 
+            // write data
             return new Promise((resolve, reject) => {
-                localdb.write(`auth/bucket/${objectId}.json`, JSON.stringify(data), (data, err) => { 
+                localdb.write(`auth/bucket/${this.sender}/${objectId}.json`, JSON.stringify(data), (data, err) => { 
                     if (err) {
                         reject(err) // reject the promise and return an error
                     } else {
-                        resolve(true) // return true
+                        resolve(objectId) // return id
+                    }
+                })
+            })
+        }
+
+        /**
+         * @func ObjectHandler.get
+         * @description Get JSON data previously uploaded to storage by using the ID
+         * 
+         * @param {string} id - The objectId of the requested object 
+         * @returns {Promise} Promise object returning the data or an error message
+         */
+        public get(id: string) {
+            return new Promise((resolve, reject) => {
+                localdb.read(`auth/bucket/${this.sender}/${id}.json`, (data, err) => { 
+                    if (err) {
+                        reject(err) // reject the promise and return an error
+                    } else {
+                        resolve(data) // return data
                     }
                 })
             })
